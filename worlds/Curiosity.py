819588f25036be8d796a286b5f882fbe06c8f171
timorl <tymorl@gmail.com>
2017-09-12 13:32:10 +0200

    Curiosity first try

---
+++ worlds/Curiosity.py
@@ -0,0 +1,15 @@
+
+from . import World
+
+class Curiosity(World):
+    def __init__(self, inner, classifier):
+        import numpy as np
+
+        def trajectories(agent, n):
+            assert n >= 1
+            assert agent==None
+            trajs = inner.trajectories(None, n)
+            _, pred = classifier.step([None]*len(trajs), [o for ((o, _, _),) in trajs])
+            return [[([0,0],o,p[0])]for p, ((o, a, _),) in zip(pred, trajs)]
+
+        self.trajectories = trajectories
