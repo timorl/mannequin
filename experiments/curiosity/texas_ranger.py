3ffdef6f53fd346ca0b6490a0d2d4afa27fedf05
timorl <tymorl@gmail.com>
2017-10-27 15:08:09 +0200

    Now actually walks
    Sometimes at least and getting ~150 points out of 300 needed.

--- experiments/curiosity/texas_ranger.py
+++ experiments/curiosity/texas_ranger.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 sys.path.append("../..")
 
 from worlds import Gym, StochasticPolicy, BaseWorld, ActionNoise, Cache
-from models import Input, Affine, LReLU, Softmax, Constant
+from models import Input, Affine, LReLU, Softmax, Tanh
 from optimizers import Adam
 from trajectories import policy_gradient, normalize, discount, print_reward, accuracy, get_rewards, replace_rewards
 
@@ -18,30 +18,12 @@ if "DEBUG" in os.environ:
     sys.excepthook = IPython.core.ultratb.FormattedTB(call_pdb=True)
 
 class Curiosity(BaseWorld):
-    def __init__(self, inner, *, classifier, history_length, plot=False):
+    def __init__(self, inner, *, classifier, history_length, for_classifier=lambda x: x, plot=None):
         history = Cache()
         classOpt = None
 
         def tag_traj(traj, tag):
-            return [(t[0], tag, 1.) for t in traj]
-
-        def plot_tagged_trajs(trajs):
-            COLORS = ["blue", "red"]
-            plt.ion()
-            plt.clf()
-            plt.grid()
-            plt.gcf().axes[0].set_ylim([-1.25,1.25])
-            plt.gcf().axes[0].set_xlim([-1.25,1.25])
-            for traj in trajs:
-                tag = traj[0][1]
-                xs, ys = [], []
-                for state, _, _ in traj:
-                    x = state[2]
-                    y = state[3]
-                    xs.append(x)
-                    ys.append(y)
-                plt.plot(xs, ys, color=COLORS[np.argmax(tag)], alpha=0.1)
-            plt.pause(0.01)
+            return [(t[0], tag, t[2]) for t in traj]
 
         def remember(agent):
             nonlocal history, classOpt
@@ -50,7 +32,7 @@ class Curiosity(BaseWorld):
             )
             classOpt = Adam(
                 np.random.randn(classifier.n_params) * 1.,
-                lr=0.08,
+                lr=0.06,
                 memory=0.9,
             )
 
@@ -61,22 +43,23 @@ class Curiosity(BaseWorld):
             oldTrajs = history.trajectories(None, n)
             innerTrajs = inner.trajectories(agent, n)
 
-            trajsForClass = (
+            trajsForClass = for_classifier(
                 [tag_traj(traj, [1,0]) for traj in oldTrajs]
                 + [tag_traj(traj, [0,1]) for traj in innerTrajs]
             )
+            trajsForClass = replace_rewards(trajsForClass, reward=lambda r: 1.)
 
-            if plot:
-                plot_tagged_trajs(trajsForClass)
+            if plot is not None:
+                plot(trajsForClass)
 
             classifier.load_params(classOpt.get_value())
             grad = policy_gradient(trajsForClass, policy=classifier)
             classOpt.apply_gradient(grad)
 
             curiosityTrajs = replace_rewards(
-                innerTrajs,
+                for_classifier(innerTrajs),
                 model=classifier,
-                reward=lambda o: o[1]
+                reward=lambda o: o[1],
             )
             return innerTrajs, curiosityTrajs
 
@@ -95,6 +78,21 @@ def combine_rewards(trajss, weights):
     rewards = np.sum(rewards, axis=0)
     return [[(o, a, r) for (o, a, _), r in zip(traj, rew)] for traj, rew in zip(trajss[0], rewards)]
 
+def change_obs_space(trajs, changer=lambda x:x):
+    return [[(changer(o, r),a,r) for o, a, r in t] for t in trajs]
+
+def interesting_part(obs, r):
+    result = (
+            obs[0],
+            obs[1],
+            obs[2],
+            obs[3],
+            obs[-12],
+            obs[-11],
+            r
+            )
+    return result
+
 STATE_SIZE = 24
 ACTION_SIZE = 4
 
@@ -103,10 +101,11 @@ def walker():
     walker = Affine(walker, 64)
     walker = LReLU(walker)
     walker = Affine(walker, ACTION_SIZE)
+    walker = Tanh(walker)
     return walker
 
 def run():
-    classifier = Input(STATE_SIZE)
+    classifier = Input(7)
     classifier = Affine(classifier, 32)
     classifier = LReLU(classifier)
     classifier = Affine(classifier, 2)
@@ -115,25 +114,52 @@ def run():
     agent = walker()
     agent.load_params(np.random.randn(agent.n_params))
 
-    world = Gym("BipedalWalker-v2", max_steps=800)
-    world = ActionNoise(world, stddev=0.05)
+    MAX_TRAIN_TIME = 200
+    trainTimeLeft = MAX_TRAIN_TIME
+    curAgentId = -1
+    curMemoryId = 0
+    def plot_tagged_trajs(trajs):
+        nonlocal trainTimeLeft, curAgentId, curMemoryId
+        COLORS = ["blue", "red"]
+        plt.ion()
+        plt.clf()
+        plt.grid()
+        plt.gcf().axes[0].set_xlim([-1.25,1.25])
+        plt.gcf().axes[0].set_ylim([-1.25,1.25])
+        plt.suptitle("Episode %d of agent %d, memories: %d"%(MAX_TRAIN_TIME-trainTimeLeft, curAgentId, curMemoryId))
+        for traj in trajs:
+            tag = traj[0][1]
+            xs, ys = [], []
+            for state, _, _ in traj:
+                x = state[2]
+                y = state[3]
+                xs.append(x)
+                ys.append(y)
+            plt.plot(xs, ys, color=COLORS[np.argmax(tag)], alpha=0.1)
+        plt.pause(0.01)
+
+
+    world = Gym("BipedalWalker-v2", max_steps=1600)
+    world = ActionNoise(world, stddev=0.2)
     world = Curiosity(
                 world,
                 classifier=classifier,
-                history_length=100,
-                plot=True
+                history_length=50,
+                for_classifier=lambda ts: change_obs_space(
+                    ts,
+                    changer=interesting_part
+                    ),
+                plot=plot_tagged_trajs
             )
-
     MAX_BOREDOM = 3
     boredom = MAX_BOREDOM
 
-    MAX_TRAIN_TIME = 500
+    MAX_MOTIVATION = 3
+    motivation = MAX_MOTIVATION
+
     agentOpt = None
-    trainTimeLeft = None
     lastScores = None
 
-    curAgentId = -1
-    curMemoryId = 0
     def memorize():
         nonlocal boredom, curMemoryId
         print("Memorizing %d..."%curMemoryId)
@@ -141,62 +167,68 @@ def run():
         boredom = MAX_BOREDOM
         curMemoryId += 1
     def reset_agent():
-        nonlocal agentOpt, trainTimeLeft, lastScores, curAgentId
+        nonlocal agentOpt, trainTimeLeft, lastScores, curAgentId, motivation
         print("Resetting agent %d."%curAgentId)
         agentOpt = Adam(
             np.random.randn(agent.n_params),
-            lr=0.03,
-            memory=0.8,
+            lr=0.05,
+            memory=0.9,
         )
         trainTimeLeft = MAX_TRAIN_TIME
         lastScores = [-0.4]
         curAgentId += 1
+        motivation = MAX_MOTIVATION
     reset_agent()
     while True:
         agent.load_params(agentOpt.get_value())
 
         realTrajs, curiosityTrajs = world.trajectories(agent, 20)
-        curScore = np.mean(get_rewards(realTrajs, episode=np.sum))/150.
+        curScore = np.mean(get_rewards(realTrajs, episode=np.sum))/300.
         lastScores.append(curScore)
         lastScores = lastScores[-10:]
         scoreDev = np.std(lastScores)
+        scoreMean = np.max([np.abs(np.mean(lastScores)),1.])
+        print(scoreDev/scoreMean)
 
         curCuriosity = np.mean(get_rewards(curiosityTrajs, episode=np.max))
 
-        print_reward(realTrajs, max_value=150.0, episode=np.sum, label="Real reward:      ")
+        print_reward(realTrajs, max_value=300.0, episode=np.sum, label="Real reward:      ")
         print_reward(curiosityTrajs, max_value=1.0, episode=np.max, label="Curiosity reward: ")
-        if curCuriosity > 0.95:
+        if curCuriosity > 0.85:
             if boredom == 0:
                 memorize()
-                if curMemoryId % 3 == 0:
-                    world.render(agent)
+                world.render(agent)
             else:
                 boredom -= 1
         else:
             boredom = np.min([boredom+1, MAX_BOREDOM])
 
-        if scoreDev < 0.01 or trainTimeLeft < 0:
-            print("Not really learning.")
-            world.render(agent)
-            if curScore < 0.01: 
-                memorize()
-                reset_agent()
-                continue
+        if scoreDev/scoreMean < 0.010 or trainTimeLeft < 0:
+            if motivation == 0:
+                print("Not really learning.")
+                world.render(agent)
+                motivation = MAX_MOTIVATION
+                trainTimeLeft = MAX_TRAIN_TIME
+                if curScore < 0.01:
+                    memorize()
+                    reset_agent()
+                    continue
+            else:
+                motivation -= 1
+        else:
+            motivation = np.min([motivation+1, MAX_MOTIVATION])
 
-        realTrajs = replace_rewards(realTrajs, episode=np.sum)
+        realTrajs = discount(realTrajs, horizon=300)
         realTrajs = normalize(realTrajs)
         curiosityTrajs = replace_rewards(curiosityTrajs, episode=np.max)
-        realWeight = np.min([(scoreDev-0.01)*6., 0.95])
+        realWeight = 0.5
         curiosityWeight = 1. - realWeight
-        print('RWeight: %f, CWeight: %f'%(realWeight, curiosityWeight))
         trajs = combine_rewards([realTrajs, curiosityTrajs], [realWeight, curiosityWeight])
         trajs = normalize(trajs)
         grad = policy_gradient(trajs, policy=agent)
         agentOpt.apply_gradient(grad)
 
         trainTimeLeft -= 1
-        if trainTimeLeft % 10 == 0:
-            print("%d episodes in."%(MAX_TRAIN_TIME-trainTimeLeft))
 
 if __name__ == "__main__":
     run()
