6f91d7ad15e993eebe1f05614ff704182694fe10
timorl <tymorl@gmail.com>
2017-11-08 19:28:52 +0100

    Removed curiosity, still sometimes learns

--- experiments/curiosity/texas_ranger.py
+++ experiments/curiosity/texas_ranger.py
@@ -17,31 +17,37 @@ if "DEBUG" in os.environ:
     import IPython.core.ultratb
     sys.excepthook = IPython.core.ultratb.FormattedTB(call_pdb=True)
 
+AGENT_VARIATIONS=5
+TRAJS_PER_VARIATION=6
+EPISODE_DELAY=5
+
 class Curiosity(BaseWorld):
-    def __init__(self, inner, *, classifier, history_length, for_classifier=lambda x: x, plot=None):
-        history = Cache()
+    def __init__(self, inner, *, classifier, for_classifier=lambda x: x, plot=None):
+        history = Cache(AGENT_VARIATIONS * TRAJS_PER_VARIATION * EPISODE_DELAY)
         classOpt = None
 
         def tag_traj(traj, tag):
             return [(t[0], tag, t[2]) for t in traj]
 
-        def remember(agent):
-            nonlocal history, classOpt
-            history.add_trajectory(
-                *inner.trajectories(agent, history_length)
-            )
+        def reset_classifier():
+            nonlocal classOpt
             classOpt = Adam(
                 np.random.randn(classifier.n_params) * 1.,
                 lr=0.06,
                 memory=0.9,
             )
 
-        def trajectories(agent, n):
+        def trajectories(agents, n):
             if classOpt is None:
-                remember(agent)
+                reset_classifier()
+                history.add_trajectories(inner.trajectories(agents[0], n))
 
-            oldTrajs = history.trajectories(None, n)
-            innerTrajs = inner.trajectories(agent, n)
+            oldTrajs, innerTrajs = [], []
+            for agent in agents:
+                oldTrajs += history.trajectories(None, n)
+                innerTrajs += inner.trajectories(agent, n)
+
+            history.add_trajectories(innerTrajs)
 
             trajsForClass = for_classifier(
                 [tag_traj(traj, [1,0]) for traj in oldTrajs]
@@ -49,12 +55,12 @@ class Curiosity(BaseWorld):
             )
             trajsForClass = replace_rewards(trajsForClass, reward=lambda r: 1.)
 
-            if plot is not None:
-                plot(trajsForClass)
-
             classifier.load_params(classOpt.get_value())
             grad = policy_gradient(trajsForClass, policy=classifier)
             classOpt.apply_gradient(grad)
+
+            if plot is not None:
+                plot(trajsForClass, classifier)
 
             curiosityTrajs = replace_rewards(
                 for_classifier(innerTrajs),
@@ -65,7 +71,7 @@ class Curiosity(BaseWorld):
 
         self.trajectories = trajectories
         self.render = inner.render
-        self.remember = remember
+        self.reset_classifier = reset_classifier
 
 from models.BaseWrapper import BaseWrapper
 
@@ -87,15 +93,13 @@ def interesting_part(obs, r):
             obs[1],
             obs[2],
             obs[3],
-            obs[-12],
-            obs[-11],
-            r
+												r
             )
     return result
 
 STATE_SIZE = 24
 ACTION_SIZE = 4
-MAX_STEPS = 200
+MAX_STEPS = 500
 
 def walker():
     walker = Input(STATE_SIZE)
@@ -106,7 +110,7 @@ def walker():
     return walker
 
 def run():
-    classifier = Input(7)
+    classifier = Input(5)
     classifier = Affine(classifier, 32)
     classifier = LReLU(classifier)
     classifier = Affine(classifier, 2)
@@ -114,19 +118,19 @@ def run():
 
     agent = walker()
     agent.load_params(np.random.randn(agent.n_params)*1.5)
+    agents = [walker() for _ in range(AGENT_VARIATIONS-1)]
 
-    MAX_TRAIN_TIME = 200
+    MAX_TRAIN_TIME = 400
     trainTimeLeft = MAX_TRAIN_TIME
     curAgentId = -1
-    curMemoryId = 0
-    def plot_tagged_trajs(trajs):
-        nonlocal trainTimeLeft, curAgentId, curMemoryId
+    def plot_tagged_trajs(trajs, classifier):
+        nonlocal trainTimeLeft, curAgentId
         COLORS = ["blue", "red"]
+        coords = np.mgrid[0:11,0:11,0:11,0:11,0:11].reshape(5, -1).T * [0.4, 0.04, 0.25, 0.25, 0.2] - [2.0, 0.2, 1.25, 1.25, 1.0]
+        classifierResults = classifier.outputs(coords)[:,1].reshape(11,11,11,11,11)
+        classifierResults = np.mean(classifierResults, axis=(0,1,4)).T[::-1,:]
         plt.clf()
-        plt.grid()
-        plt.gcf().axes[0].set_xlim([-1.25,1.25])
-        plt.gcf().axes[0].set_ylim([-1.25,1.25])
-        plt.suptitle("Episode %d of agent %d, memories: %d"%(MAX_TRAIN_TIME-trainTimeLeft, curAgentId, curMemoryId))
+        plt.suptitle("Episode %d of agent %d"%(MAX_TRAIN_TIME-trainTimeLeft, curAgentId))
         for traj in trajs:
             tag = traj[0][1]
             xs, ys = [], []
@@ -136,6 +140,7 @@ def run():
                 xs.append(x)
                 ys.append(y)
             plt.plot(xs, ys, color=COLORS[np.argmax(tag)], alpha=0.1)
+        plt.imshow(classifierResults, zorder=0, aspect="auto", vmin=0.0, vmax=1.0, cmap="gray", interpolation="bicubic", extent=[-1.25, 1.25, -1.25, 1.25])
         plt.gcf().set_size_inches(10, 8)
         plt.gcf().savefig(
             "__step_a%03d_t%03d.png" %
@@ -149,7 +154,6 @@ def run():
     world = Curiosity(
                 world,
                 classifier=classifier,
-                history_length=50,
                 for_classifier=lambda ts: change_obs_space(
                     ts,
                     changer=interesting_part
@@ -159,11 +163,6 @@ def run():
 
     agentOpt = None
 
-    def memorize():
-        nonlocal curMemoryId
-        print("Memorizing %d..."%curMemoryId)
-        world.remember(agent)
-        curMemoryId += 1
     def save_agent():
         np.save(
             "__ranger_a%03d_t%03d.npy" %
@@ -172,8 +171,6 @@ def run():
         )
     def reset_agent():
         nonlocal agentOpt, trainTimeLeft, curAgentId
-        if agentOpt is not None:
-            save_agent()
         print("Resetting agent %d."%curAgentId)
         agentOpt = Adam(
             np.random.randn(agent.n_params)*1.5,
@@ -184,28 +181,29 @@ def run():
         curAgentId += 1
     reset_agent()
     while True:
-        agent.load_params(agentOpt.get_value())
+        parameters = agentOpt.get_value()
 
-        realTrajs, curiosityTrajs = world.trajectories(agent, 30)
+        agent.load_params(parameters)
+        for i in range(AGENT_VARIATIONS-1):
+            agents[i].load_params(parameters + np.random.randn(*parameters.shape)*0.9)
+        realTrajs, curiosityTrajs = world.trajectories(agents+[agent], TRAJS_PER_VARIATION)
 
-        print_reward(realTrajs, max_value=300.0, episode=np.sum, label="Real reward:      ")
+        print_reward(realTrajs, max_value=100.0, episode=np.sum, label="Real reward:    ")
         print_reward(curiosityTrajs, max_value=1.0, episode=np.max, label="Curiosity reward: ")
         if trainTimeLeft % 20 == 0:
             save_agent()
-            memorize()
 
         if trainTimeLeft < 0:
             print("Timeout.")
             save_agent()
             trainTimeLeft = MAX_TRAIN_TIME
-            memorize()
             reset_agent()
             continue
 
         realTrajs = discount(realTrajs, horizon=200)
         realTrajs = normalize(realTrajs)
         curiosityTrajs = replace_rewards(curiosityTrajs, episode=np.max)
-        realWeight = 0.5
+        realWeight = 1.
         curiosityWeight = 1. - realWeight
         trajs = combine_rewards([realTrajs, curiosityTrajs], [realWeight, curiosityWeight])
         trajs = normalize(trajs)
